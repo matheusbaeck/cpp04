6dde0ac34974d496b0996236f1f9a3c13de84714
matheusbaeck <matheusbaeck@gmail.com>
2024-06-18 20:27:51 +0200

    ex01 changing main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:59:11 by math              #+#    #+#             */
-/*   Updated: 2024/06/18 20:09:44 by math             ###   ########.fr       */
+/*   Updated: 2024/06/18 20:27:19 by math             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 int	main( void )
 {
 
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete j;
-	delete i;
+	// const Animal* j = new Dog();
+	// const Animal* i = new Cat();
+	// delete j;
+	// delete i;
 
-	size_t size = 2;
-	const Animal* arr[2];
+	size_t size = 5;
+	const Animal* arr[size];
 	
 	for (size_t i = 0; i < size; i++)
 	{
@@ -32,13 +32,12 @@ int	main( void )
 		else
 			arr[i] = new Dog();
 	}
+
 	std::cout << std::endl;
 	for (size_t i = 0; i < size; i++)
-	{
 		std::cout << "\t" << *arr[i];
-		
-	}
 	std::cout << std::endl;
+
 	for (size_t i = 0; i < size; i++)
 		delete arr[i];	
 	return (0);
